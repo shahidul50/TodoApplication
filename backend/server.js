c7e33563cfe24589fea3e9b30a656b42f915e79e
Shahidul <shahid.cse.19@gmail.com>
2023-08-24 10:54:48 +0600

    Simple Change in Server.js file

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use('/api/todos', todoRoutes)
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, './frontend/dist')));
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
   
     app.get('*', (req, res) =>
       res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
